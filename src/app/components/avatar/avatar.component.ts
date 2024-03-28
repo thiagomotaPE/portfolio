b35fb165bf79715ef89d0b5ba352817eeafec436
Thiago Mota <thiagomotape@hotmail.com.br>
2024-03-27 21:03:58 -0300

    fiz a animação de alguns items da home

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './avatar.component.css'
 })
 export class AvatarComponent {
+  public colorCodeIcon = '';
+  changeCodeIconColor() {
+    this.colorCodeIcon = this.getRandomColor();
+  }
 
+  private getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }
